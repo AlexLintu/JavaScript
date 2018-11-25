365ff56c5fb2f7d30fd03a02a07d3adad00f358e
Alexandra Tkachenko <osatkachenko@gmail.com>
2018-11-24 19:38:20 -0800

    Ternary Operator

---
+++ codeCademy/9-ternary-operator.js
@@ -0,0 +1,38 @@
+// Example #1
+// if...else
+let isLocked = false;
+
+if (isLocked) {
+  console.log('You will need a key to open the door.');
+} else {
+  console.log('You will not need a key to open the door.');
+}
+
+// Ternary:
+isLocked ? console.log('You will need a key to open the door.') : console.log('You will not need a key to open the door.');
+
+// Example #2
+// if...else
+let isCorrect = true;
+
+if (isCorrect) {
+  console.log('Correct!');
+} else {
+  console.log('Incorrect!');
+}
+
+// Ternary:
+isCorrect ? console.log('Correct!') : console.log('Incorrect!');
+
+// Example #3
+// if...else
+let favoritePhrase = 'Love That!';
+
+if (favoritePhrase === 'Love That!') {
+  console.log('I love that!');
+} else {
+  console.log("I don't love that!");
+}
+
+// Ternary:
+favoritePhrase === 'Love That!' ? console.log('I love that!') : console.log("I don't love that!");
