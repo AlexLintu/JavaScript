5b5d7915bbd8b97fdfee151b86c320fff853fa13
Alexandra Tkachenko <osatkachenko@gmail.com>
2018-11-26 19:32:03 -0800

    Added Arrow Function

---
+++ codeCademy/16-arrow-functions.js
@@ -0,0 +1,17 @@
+// Function Expression syntax:
+const plantNeedsWater = function(day) {
+  if (day === 'Wednesday') {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+// Arrow function syntax:
+const plantNeedsWater = (day) => {
+  if (day === 'Wednesday') {
+    return true;
+  } else {
+    return false;
+  }
+};
