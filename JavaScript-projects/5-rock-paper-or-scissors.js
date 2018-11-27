9daa9470b7240e9697b1cf3c5faf58c83fefd430
Alexandra Tkachenko <osatkachenko@gmail.com>
2018-11-26 22:02:36 -0800

    Completed the game

---
+++ JavaScript-projects/5-rock-paper-or-scissors.js
@@ -0,0 +1,65 @@
+const getUserChoice = userInput => {
+  userInput = userInput.toLowerCase();
+  if (userInput === 'rock' || userInput === 'paper' || userInput === 'scissors' || userInput === 'bomb') {
+    return userInput;
+  } else {
+    console.log('Please enter rock, paper or scissors.')
+  }
+};
+
+const getComputerChoice = () => {
+  let randomNum = Math.floor(Math.random() * 3);
+  if (randomNum === 0) {
+    return 'rock';
+  } else if (randomNum === 1) {
+    return 'paper';
+  } else if (randomNum === 2) {
+    return 'scissors';
+  } else {
+    return 'error!'
+  }
+}
+
+const determineWinner = (userChoice, computerChoice) => {
+  if (userChoice === computerChoice) {
+    return 'The game is a tie!';
+  }
+  
+  if (userChoice === 'bomb') {
+    return 'You won!!!';
+  }
+
+  if (userChoice === 'rock') {
+    if (computerChoice === 'paper') {
+      return 'The computer won!';
+    } else {
+      return 'You won!';
+    }
+  }
+
+  if (userChoice === 'scissors') {
+    if (computerChoice === 'rock') {
+      return 'The computer won!';
+    } else {
+      return 'You won!';
+    }
+  }
+
+  if (userChoice === 'paper') {
+    if (computerChoice === 'scissors') {
+      return 'The computer won!';
+    } else {
+      return 'You won!';
+    }
+  }
+}
+
+function playGame() {
+  let userChoice = getUserChoice('paper');
+  console.log(userChoice);
+  let computerChoice = getComputerChoice();
+  console.log(computerChoice);
+  console.log(determineWinner(userChoice, computerChoice));
+}
+
+playGame();
