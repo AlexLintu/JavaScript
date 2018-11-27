650619d6fae3e875fa5d07f05bca116e4b3bfd89
Alexandra Tkachenko <osatkachenko@gmail.com>
2018-11-27 10:25:43 -0800

    Adjusted first switch statement

--- JavaScript-projects/6-sleep-debt-calculator.js
+++ JavaScript-projects/6-sleep-debt-calculator.js
@@ -3,21 +3,30 @@ const getSleepHours = day => {
   switch (day) {
     case 'monday':
       return 7;
+      break;
     case 'tuesday':
       return 8;
+      break;
     case 'wednesday':
       return 6;
+      break;
     case 'thursday':
       return 7;
+      break;
     case 'friday':
       return 8;
+      break;
     case 'saturday':
       return 9;
+      break;
     case 'sunday':
       return 9;
+      break;
+    default:
+      return 'Error! Please enter valid day name.';
+      break;
   }
 }
-console.log(getSleepHours('monday'));
 
 // Calculating actual hours of sleep per week:
 const getActualSleepHours = () => {
@@ -26,15 +35,13 @@ const getActualSleepHours = () => {
 console.log('Actual hours of sleep: ' + getActualSleepHours());
 
 // Calculating ideal hours of sleep per week:
-const getIdealSleepHours = () => {
-  const idealHours = 8;
-  return idealHours * 7;
-}
-console.log('Ideal hours of sleep: ' + getIdealSleepHours());
+const getIdealSleepHours = idealHours => idealHours * 7;
+console.log('Ideal hours of sleep: ' + getIdealSleepHours(8));
 
+// Calculating sleep debt per week:
 const calculateSleepDebt = () => {
   const actualSleepHours = getActualSleepHours();
-  const idealSleepHours = getIdealSleepHours();
+  const idealSleepHours = getIdealSleepHours(8);
 
   if (actualSleepHours === idealSleepHours) {
     console.log(`You've got perfect amount of sleep: ${actualSleepHours} hours!`);
