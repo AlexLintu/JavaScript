463c8bf1c24afd7e9146381cc0ac677fa9b8aed8
Alexandra Tkachenko <osatkachenko@gmail.com>
2018-11-27 18:35:19 -0800

    Adjusted formatting

--- JavaScript-projects/12-tip-calculator.js
+++ JavaScript-projects/12-tip-calculator.js
@@ -1,20 +1,20 @@
 const tipCalculator = (quality, total) => {
-  switch(quality) {
+  switch (quality) {
     case 'bad':
-    return total * 0.05;
-    break;
+      return total * 0.05;
+      break;
     case 'ok':
-    return total * 0.15;
-    break;
+      return total * 0.15;
+      break;
     case 'good':
-    return total * 0.2;
-    break;
+      return total * 0.2;
+      break;
     case 'excellent':
-    return total * 0.3;
-    break;
+      return total * 0.3;
+      break;
     default:
-    return total * 0.18;
-    break;
+      return total * 0.18;
+      break;
   }
 }
 
