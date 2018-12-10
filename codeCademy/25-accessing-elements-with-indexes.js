20f2ea63ad470affe2ebda580027de5e65032ffc
Alexandra Tkachenko <osatkachenko@gmail.com>
2018-12-10 09:51:14 -0800

    Array index

---
+++ codeCademy/25-accessing-elements-with-indexes.js
@@ -0,0 +1,8 @@
+const famousSayings = ['Fortune favors the brave.', 'A joke is a very serious thing.', 'Where there is love there is life.'];
+
+console.log(famousSayings[2]); // Where there is love there is life.
+console.log(famousSayings[3]); // undefined
+
+// Saved an array element into a new variable:
+let listItem = famousSayings[0];
+console.log(listItem); // Fortune favors the brave.
