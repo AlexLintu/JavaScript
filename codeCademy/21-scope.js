002a487c31bbf66d49ffc076eb22b8d38c949126
Alexandra Tkachenko <osatkachenko@gmail.com>
2018-12-09 19:41:31 -0800

    Added block scope example

--- codeCademy/21-scope.js
+++ codeCademy/21-scope.js
@@ -1,6 +1,7 @@
 // Global scope variable, which can be accessed by any code in the program:
 const city = 'New York City';
 const logCitySkyline = () => {
+  // Block scope local variable, which is only accessible to the lines of code within the block {}:
   let skyscraper = 'Empire State Building';
   return 'The stars over the ' + skyscraper + ' in ' + city;
 }
---
+++ codeCademy/23-block-scope.js
@@ -0,0 +1,7 @@
+const logVisibleLightWaves = () => {
+  const lightWaves = 'Moonlight';
+  console.log(lightWaves); // Moonlight
+}
+
+logVisibleLightWaves(); // Moonlight 
+console.log(lightWaves); // ReferenceError
