7809de2a8b2ca6c1882c0a5d181e63017624beb0
Alexandra Tkachenko <osatkachenko@gmail.com>
2018-12-11 10:09:05 -0800

    Added for loops

---
+++ codeCademy/29-for-loops.js
@@ -0,0 +1,9 @@
+// Regular for loop:
+for (let i = 5; i < 11; i++) {
+  console.log(i); // 5 6 7 8 9 10
+};
+
+// Reverse for loop:
+for (let counter = 3; counter >= 0; counter--) {
+  console.log(counter); // 3 2 1 0
+};
