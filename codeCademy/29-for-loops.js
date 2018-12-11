91d6e159e9f384964b0620bb63a1cf189d1318b3
Alexandra Tkachenko <osatkachenko@gmail.com>
2018-12-11 10:16:35 -0800

    Added looping through arrays example

--- codeCademy/29-for-loops.js
+++ codeCademy/29-for-loops.js
@@ -7,3 +7,10 @@ for (let i = 5; i < 11; i++) {
 for (let counter = 3; counter >= 0; counter--) {
   console.log(counter); // 3 2 1 0
 };
+
+// Looping through an array:
+const vacationSpots = ['Bali', 'Paris', 'Tulum'];
+
+for (let i = 0; i < vacationSpots.length; i++) {
+  console.log('I would love to visit ' + vacationSpots[i]); // I would love to visit Bali I would love to visit Paris I would love to visit Tulum
+}
