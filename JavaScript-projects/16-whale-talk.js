9be8654e0c3209f82ac0900af8cc546515588917
Alexandra Tkachenko <osatkachenko@gmail.com>
2018-12-11 12:43:20 -0800

    Added comments

--- JavaScript-projects/16-whale-talk.js
+++ JavaScript-projects/16-whale-talk.js
@@ -2,8 +2,8 @@ const input = 'Hi, Human';
 const vowels = ['a', 'e', 'i', 'o', 'u'];
 const resultArray = [];
 
-for (let i = 0; i < input.length; i++) {
-  for (let j = 0; j < vowels.length; j++) {
+for (let i = 0; i < input.length; i++) { // 'i' is inputIndex
+  for (let j = 0; j < vowels.length; j++) { // 'j' is vowelsIndex
     if (input[i].toLowerCase() === vowels[j]) {
       resultArray.push(input[i]);
       if (input[i].toLowerCase() === 'e' || input[i].toLowerCase() === 'u') {
