b0e2e0683416039ba24aa835636c5f6a733e5d22
Alexandra Tkachenko <osatkachenko@gmail.com>
2018-12-17 11:45:46 -0800

    Added dot notation example

---
+++ codeCademy/41-accessing-object-properties.js
@@ -0,0 +1,10 @@
+let spaceship = {
+  homePlanet: 'Earth',
+  color: 'silver',
+  'Fuel Type': 'Turbo Fuel',
+  numCrew: 5,
+  flightPath: ['Venus', 'Mars', 'Saturn']
+};
+
+const crewCount = spaceship.numCrew; // 5
+const planetArray = spaceship.flightPath; // ['Venus', 'Mars', 'Saturn']
