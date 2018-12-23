a269506b7774d46022b8d111fa4f17046452e279
Alexandra Tkachenko <osatkachenko@gmail.com>
2018-12-22 22:47:09 -0800

    Added methods

--- JavaScript-projects/18-meal-maker.js
+++ JavaScript-projects/18-meal-maker.js
@@ -32,6 +32,27 @@ const menu = {
   },
   getRandomDishFromCourse(courseName) {
     const dishes = this._courses[courseName];
-    const randomIndex = 
+    const randomIndex = Math.floor(Math.random() * dishes.length);
+    return dishes[randomIndex];
+  },
+  generateRandomMeal() {
+    const appetizer = this.getRandomDishFromCourse('appetizers');
+    const main = this.getRandomDishFromCourse('mains');
+    const dessert = this.getRandomDishFromCourse('desserts');
+    const totalPrice = appetizer.price + main.price + dessert.price;
+    return `Your meal is ${appetizer.name}, ${main.name}, ${dessert.name} and the total cost is ${totalPrice}`;
   }
 }
+
+menu.addDishToCourse('appetizers', 'Salad', 5);
+menu.addDishToCourse('appetizers', 'Soup', 10);
+menu.addDishToCourse('appetizers', 'Chips', 20);
+menu.addDishToCourse('mains', 'Stake', 30);
+menu.addDishToCourse('mains', 'Pasta', 40);
+menu.addDishToCourse('mains', 'Sushi', 50);
+menu.addDishToCourse('desserts', 'Cake', 60);
+menu.addDishToCourse('desserts', 'Pie', 70);
+menu.addDishToCourse('desserts', 'Cookies', 80);
+
+const meal = menu.generateRandomMeal;
+console.log(meal);
