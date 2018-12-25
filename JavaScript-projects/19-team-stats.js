1b9405841ae6f6f6a15f4763ce13165b41afa2ba
Alexandra Tkachenko <osatkachenko@gmail.com>
2018-12-24 21:34:45 -0800

    Added players and games

--- JavaScript-projects/19-team-stats.js
+++ JavaScript-projects/19-team-stats.js
@@ -1,4 +1,36 @@
 const team = {
-  _players: [],
-  _games: []
+  _players: [
+    {
+      firstName: 'Lionel',
+      lastName: 'Messi',
+      age: 31
+    },
+    {
+      firstName: 'David',
+      lastName: 'Beckham',
+      age: 43
+    },
+    {
+      firstName: 'Zinedine',
+      lastName: 'Zidane',
+      age: 46
+    }
+  ],
+  _games: [
+    {
+      opponent: 'Opponent1',
+      teamPoints: 10,
+      opponentPoints: 20
+    },
+    {
+      opponent: 'Opponent2',
+      teamPoints: 30,
+      opponentPoints: 40
+    },
+    {
+      opponent: 'Opponent3',
+      teamPoints: 50,
+      opponentPoints: 60
+    }
+  ]
 }
