c8f6d9def1a327f4725e3694110d3c848008ec82
Alexandra Tkachenko <osatkachenko@gmail.com>
2018-12-27 11:38:55 -0800

    Added greet alients project

---
+++ JavaScript-projects/21-greet-alients.js
@@ -0,0 +1,20 @@
+/* Regular for loop:
+const greetAliens = arr => {
+  for (let i=0; i < arr.length; i++) {
+    console.log(`Oh powerful ${arr[i]}, we humans offer our unconditional surrender!`);
+  }
+} */
+
+// Same result with the .forEach() method:
+const greetAliens = arr => arr.forEach(name => console.log(`Oh powerful ${name}, we humans offer our unconditional surrender!`));
+
+const aliens = ["Blorgous", "Glamyx", "Wegord", "SpaceKing"];
+
+greetAliens(aliens);
+
+/* Output:
+Oh powerful Blorgous, we humans offer our unconditional surrender!
+Oh powerful Glamyx, we humans offer our unconditional surrender!
+Oh powerful Wegord, we humans offer our unconditional surrender!
+Oh powerful SpaceKing, we humans offer our unconditional surrender!
+*/
