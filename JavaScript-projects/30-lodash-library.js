d5fe17e3f559a51727d0a3b6160f5f4ed26884f4
Alexandra Tkachenko <osatkachenko@gmail.com>
2018-12-28 18:07:55 -0800

    added inRange method

--- JavaScript-projects/30-lodash-library.js
+++ JavaScript-projects/30-lodash-library.js
@@ -3,11 +3,32 @@ const _ = {
     let lowerClampedValue = Math.max(number, lower);
     let clampedValue = Math.min(lowerClampedValue, upper);
     return clampedValue;
+  },
+  inRange(number, start, end) {
+    if (end === undefined) {
+      end = start;
+      start = 0;
+    }
+    if (start > end) {
+      let temp = end;
+      end = start;
+      start = temp;
+    }
+    let isInRange = number > start && number < end;
+    return isInRange;
   }
 }
 
+// .clamp() tests:
 console.log(_.clamp(3, 2, 4)); // 3
 console.log(_.clamp(1, 2, 4)); // 2
 console.log(_.clamp(5, 2, 4)); // 4
+// .inRange() tests:
+console.log(_.inRange(3, 2, 4)); //  true
+console.log(_.inRange(1, 2, 4)); // false
+console.log(_.inRange(5, 2, 4)); // false
+console.log(_.inRange(7, 2)); // false
+console.log(_.inRange(3, 5, 2)); // true
+
 // Do not write or modify code below this line.
 module.exports = _;
