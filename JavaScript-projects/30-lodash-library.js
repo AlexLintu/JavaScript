f97aebc0dfdabe5277ba0fbd3c910a7dc8c49675
Alexandra Tkachenko <osatkachenko@gmail.com>
2018-12-29 19:59:05 -0800

    created pad method

--- JavaScript-projects/30-lodash-library.js
+++ JavaScript-projects/30-lodash-library.js
@@ -17,9 +17,18 @@ const _ = {
     let isInRange = number > start && number < end;
     return isInRange;
   },
-  words(str) {
-    let arrOfWords = str.split(' ');
+  words(string) {
+    let arrOfWords = string.split(' ');
     return arrOfWords;
+  },
+  pad(string, targetLength) {
+    if (targetLength <= string.length) {
+      return string;
+    }
+    let paddingStart = Math.floor((targetLength - string.length) / 2);
+    let paddingEnd = targetLength - string.length - paddingStart;
+    let paddedString = ' '.repeat(paddingStart) + string + ' '.repeat(paddingEnd);
+    return paddedString;
   }
 }
 
@@ -33,8 +42,12 @@ console.log(_.inRange(1, 2, 4)); // false
 console.log(_.inRange(5, 2, 4)); // false
 console.log(_.inRange(7, 2)); // false
 console.log(_.inRange(3, 5, 2)); // true
-//.words() tests:
+// .words() tests:
 console.log(_.words('Hello there!')); // [ 'Hello', 'there!' ]
+// .pad() tests:
+console.log(_.pad('Hello', 10));
+console.log(_.pad('Hello', 7));
+console.log(_.pad('Hello', 2));
 
 // Do not write or modify code below this line.
 module.exports = _;
