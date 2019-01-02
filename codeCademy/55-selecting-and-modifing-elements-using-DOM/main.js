19b0ec27a9085b1b7b7e7fff18d881a9ab90d6e2
Alexandra Tkachenko <osatkachenko@gmail.com>
2019-01-02 14:20:52 -0800

    Added examples of querySelector and getElementById

---
+++ codeCademy/55-selecting-and-modifing-elements-using-DOM/main.js
@@ -0,0 +1,4 @@
+document.querySelector('h1').innerHTML = 'Most popular TV show searches in 2016';
+// Two ways to adjust the element by ID: 
+document.querySelector('#fourth').innerHTML = 'Fourth element';
+document.getElementById('fourth').innerHTML = 'Fourth element';
