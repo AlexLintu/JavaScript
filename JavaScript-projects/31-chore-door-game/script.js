74e6ddf8e62a281510b05c36c4524d401d498a34
Alexandra Tkachenko <osatkachenko@gmail.com>
2019-01-03 13:54:03 -0800

    Added random door logic

--- JavaScript-projects/31-chore-door-game/script.js
+++ JavaScript-projects/31-chore-door-game/script.js
@@ -1,11 +1,38 @@
-let doorImage1 = document.getElementById('door1');
+// Logic for clicking on the doors and seeing images:
+const doorImage1 = document.getElementById('door1');
 let botDoorPath = 'https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/robot.svg';
 doorImage1.onclick = () => { doorImage1.src = botDoorPath };
 
-let doorImage2 = document.getElementById('door2');
-let beachDoorPath = 'https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/beach.svg';
+const doorImage2 = document.getElementById('door2');
+const beachDoorPath = 'https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/beach.svg';
 doorImage2.onclick = () => { doorImage2.src = beachDoorPath };
 
-let doorImage3 = document.getElementById('door3');
-let spaceDoorPath = 'https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/space.svg';
+const doorImage3 = document.getElementById('door3');
+const spaceDoorPath = 'https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/space.svg';
 doorImage3.onclick = () => { doorImage3.src = spaceDoorPath };
+
+// Logic for randomizing the game:
+const numClosedDoors = 3;
+let openDoor1;
+let openDoor2;
+let openDoor3;
+
+const randomChoreDoorGenerator = () => {
+  let choreDoor = Math.floor(Math.random() * numClosedDoors);
+
+  if (choreDoor === 0) {
+    openDoor1 = botDoorPath;
+    openDoor2 = beachDoorPath;
+    openDoor3 = spaceDoorPath;
+  } else if (choreDoor === 1) {
+    openDoor1 = beachDoorPath;
+    openDoor2 = botDoorPath;
+    openDoor3 = spaceDoorPath;
+  } else {
+    openDoor1 = spaceDoorPath;
+    openDoor2 = beachDoorPath;
+    openDoor3 = botDoorPath;
+  }
+}
+
+
