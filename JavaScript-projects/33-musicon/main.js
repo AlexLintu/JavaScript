a4bbd0f4dd4a35ed5c61d4ebb39944b33a40d64b
Alexandra Tkachenko <osatkachenko@gmail.com>
2019-01-14 22:05:06 -0800

    Added HandlebarsJS to the main file

--- JavaScript-projects/33-musicon/main.js
+++ JavaScript-projects/33-musicon/main.js
@@ -30,3 +30,11 @@ const context = {
     }
   ]
 };
+
+const templateElement = document.getElementById('templateHB');
+const templateSource = templateElement.innerHTML;
+
+const template = Handlebars.compile(templateSource)
+
+const compiledHtml = template(context);
+document.getElementById('information').innerHTML = compiledHtml;
