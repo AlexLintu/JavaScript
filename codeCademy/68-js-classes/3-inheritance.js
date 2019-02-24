0be83437f4582efecb7b261befcb01e13d53766b
Alexandra Tkachenko <osatkachenko@gmail.com>
2019-02-24 12:57:27 -0500

    Added super class

---
+++ codeCademy/68-js-classes/3-inheritance.js
@@ -0,0 +1,19 @@
+// HospitalEmployee is a super class that contains shared properties of sub-classes Doctor & Nurse
+class HospitalEmployee {
+  constructor(name) {
+    this._name = name;
+    this._remainingVacationDays = 20;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get remainingVacationDays() {
+    return this._remainingVacationDays;
+  }
+
+  takeVacationDays(daysOff) {
+    this._remainingVacationDays = this._remainingVacationDays - daysOff;
+  }
+}
