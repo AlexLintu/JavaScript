2b572330dd12f4c8cce6b7668a1700810d5279d3
Alexandra Tkachenko <osatkachenko@gmail.com>
2019-02-27 13:35:53 -0500

    Added second sub-class

--- JavaScript-projects/34-buid-a-library/main.js
+++ JavaScript-projects/34-buid-a-library/main.js
@@ -1,4 +1,4 @@
-// SUPER CLASS:
+// -- 1. SUPER CLASS `Media` --:
 class Media {
   // `title` argument sets the one property that is in all three subclasses of Media, and does not have a default value:
   constructor(title) {
@@ -23,20 +23,14 @@ class Media {
     return this._ratings;
   }
 
-  // toggleCheckOutStatus() {
-  //   if (this._isCheckedOut) {
-  //     return this._isCheckedOut = false;
-  //   } else {
-  //     return this._isCheckedOut = true;
-  //   }
-  // }
-
   toggleCheckOutStatus() {
     return this._isCheckedOut = !this._isCheckedOut;
   }
 
   getAverageRating() {
-    const result = (this._ratings.reduce((currentSum, rating) => currentSum + rating, 0)) / this._ratings.length;
+    const ratingsSum = this._ratings.reduce((currentSum, rating) => currentSum + rating, 0);
+    const ratingsLength = this._ratings.length;
+    const result = ratingsSum / ratingsLength;
     return result;
   }
 
@@ -45,11 +39,11 @@ class Media {
   }
 }
 
-// SUB-CLASSES:
+// -- 2. SUB-CLASSES --:
 class Book extends Media {
   // Arguments that do not have default values:
-  constructor(title, author, pages) {
-    super(title, ratings);
+  constructor(author, title, pages) {
+    super(title);
     this._author = author;
     this._pages = pages;
   }
@@ -64,7 +58,7 @@ class Book extends Media {
 }
 
 class Movie extends Media {
-  constructor(title, director, runTime) {
+  constructor(director, title, runTime) {
     super(title);
     this._director = director;
     this._runTime = runTime;
@@ -79,15 +73,33 @@ class Movie extends Media {
   }
 }
 
-// INSTANCES:
-const historyOfEverything = new Book('A Short History of Nearly Everything', 'Bill Bryson', 544);
+// -- 3. INSTANCES --:
+const historyOfEverything = new Book('Bill Bryson', 'A Short History of Nearly Everything', 544);
 
+// Changing checkout status:
 historyOfEverything.toggleCheckOutStatus();
 console.log(historyOfEverything.isCheckedOut);
 
+// Adding ratings:
 historyOfEverything.addRating(4);
 historyOfEverything.addRating(5);
 historyOfEverything.addRating(5);
 
-historyOfEverything.getAverageRating();
-console.log(historyOfEverything.ratings);
+// Getting an average rating value:
+console.log(historyOfEverything.getAverageRating());
+
+
+// MOVIE Instance:
+const speed = new Movie('Jan de Bont', 'Speed', 116);
+
+// Changing checkout status:
+speed.toggleCheckOutStatus();
+console.log(speed.isCheckedOut);
+
+// Adding ratings:
+speed.addRating(1);
+speed.addRating(1);
+speed.addRating(5);
+
+// Getting an average rating value:
+console.log(speed.getAverageRating());
