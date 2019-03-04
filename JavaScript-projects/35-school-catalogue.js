455ef408b76f74ac411b52656d69f91687ae9f3f
Alexandra Tkachenko <osatkachenko@gmail.com>
2019-03-03 21:27:55 -0800

    Added properties and getters for superclass

--- JavaScript-projects/35-school-catalogue.js
+++ JavaScript-projects/35-school-catalogue.js
@@ -1,3 +1,19 @@
-class test {
+class School {
+  constructor(name, numberOfStudents) {
+    this._name = name;
+    this._level = ['primary', 'middle', 'high'];
+    this._numberOfStudents = numberOfStudents;
+  }
 
+  get name() {
+    return this._name;
+  }
+
+  get level() {
+    return this._level;
+  }
+
+  get numberOfStudents() {
+    return this._numberOfStudents;
+  }
 }
