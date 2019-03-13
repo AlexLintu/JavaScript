64fc2079725cacdb44df89258d925e3cecaa4591
Alexandra Tkachenko <osatkachenko@gmail.com>
2019-03-12 19:42:39 -0700

    Added named exports

--- JavaScript-projects/36-message-mixer/message.js
+++ JavaScript-projects/36-message-mixer/message.js
@@ -1,15 +1,17 @@
-import MessageMixer from './message-mixer.js';
-// const MessageMixer = require('./message-mixer.js');
+// 1. const MessageMixer = require('./message-mixer.js');
+// 2. import MessageMixer from './message-mixer.js';
+
+import { countCharacter, capitalizeFirstCharacterOfWords, reverseWord, reverseAllWords, replaceFirstOccurence, replaceAllOccurrences, encode, palindrome, pigLatin } from './message-mixer.js';
 
 function displayMessage() {
-  console.log(MessageMixer.capitalizeFirstCharacterOfWords("What is the color of the sky?"));
-  console.log(MessageMixer.countCharacter("What is the color of the sky?", "t"));
-  console.log(MessageMixer.reverseWord("What is the color of the sky?"));
-  console.log(MessageMixer.reverseAllWords("What is the color of the sky?"));
-  console.log(MessageMixer.replaceFirstOccurence("What is the color of the sky?", "sky", "water"));
-  console.log(MessageMixer.encode("What is the color of the sky?"));
-  console.log(MessageMixer.palindrome("What is the color of the sky?")); // What is the color of the sky? ?yks eht fo roloc eht si tahW
-  console.log(MessageMixer.pigLatin("What is the color of the sky?", 't')); // Whattistthetcolortoftthetsky?
+  console.log(capitalizeFirstCharacterOfWords("What is the color of the sky?"));
+  console.log(countCharacter("What is the color of the sky?", "t"));
+  console.log(reverseWord("What is the color of the sky?"));
+  console.log(reverseAllWords("What is the color of the sky?"));
+  console.log(replaceFirstOccurence("What is the color of the sky?", "sky", "water"));
+  console.log(encode("What is the color of the sky?"));
+  console.log(palindrome("What is the color of the sky?")); // What is the color of the sky? ?yks eht fo roloc eht si tahW
+  console.log(pigLatin("What is the color of the sky?", 't')); // Whattistthetcolortoftthetsky?
 
 }
 
