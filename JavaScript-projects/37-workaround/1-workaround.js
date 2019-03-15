2cffc550eaca609012da0ed7337d016d14eb15c9
Alexandra Tkachenko <osatkachenko@gmail.com>
2019-03-15 11:52:11 -0700

    Completed the first module export

--- JavaScript-projects/37-workaround/1-workaround.js
+++ JavaScript-projects/37-workaround/1-workaround.js
@@ -10,5 +10,22 @@ function getEmployeeInformation(inputSalary) {
 }
 
 getEmployeeInformation(10000);
+// Cadre: entryLevel
+// Tax: 500
+// Benefits: health
+// Bonus: 200
+// Reimbursement Eligibility: 5000
+
 getEmployeeInformation(50000);
+// Cadre: midLevel
+// Tax: 5000
+// Benefits: health, housing
+// Bonus: 1000
+// Reimbursement Eligibility: 13000
+
 getEmployeeInformation(100000);
+// Cadre: seniorLevel
+// Tax: 20000
+// Benefits: health, housing, wellness, gym
+// Bonus: 2000
+// Reimbursement Eligibility: 31000
