81b2f8d1b269c845a79b53df5522dfcd59e50073
Alexandra Tkachenko <osatkachenko@gmail.com>
2019-03-16 19:51:43 -0700

    Fixed the method call

---
+++ codeCademy/70-debugging-js-errors/4-finding-documentation.js
@@ -0,0 +1,6 @@
+// Link to String.repeat() documentation: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/repeat
+function doubleString(string) {
+  return string.repeat(2);
+}
+
+console.log("doubleString('echo') returns: " + doubleString('echo')); // doubleString('echo') returns: echoecho
