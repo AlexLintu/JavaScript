f3d856f73356ff91a4970845795e1f638b958dfc
Web Intern <web.intern@canucks.com>
2019-03-26 16:12:28 -0700

    onFulfilled & onRejected functions

---
+++ codeCademy/72-js-promises/3-onFulfilled-and-onrejected/app.js
@@ -0,0 +1,19 @@
+const {
+  checkInventory
+} = require('./library.js');
+
+const order = [
+  ['sunglasses', 1],
+  ['bags', 2]
+];
+
+const handleSuccess = (resolvedValue) => {
+  console.log(resolvedValue);
+};
+
+const handleFailure = (rejectionReason) => {
+  console.log(rejectionReason);
+};
+
+// checkInventory(order) is a promise
+checkInventory(order).then(handleSuccess, handleFailure);
