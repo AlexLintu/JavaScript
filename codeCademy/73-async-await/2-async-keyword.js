852ca3645e4046fb0e0a5424cba79e16e637b089
Web Intern <web.intern@canucks.com>
2019-03-27 14:22:16 -0700

    using async keyword

---
+++ codeCademy/73-async-await/2-async-keyword.js
@@ -0,0 +1,41 @@
+// 1. Regular Promise:
+function withConstructor(num) {
+  return new Promise((resolve, reject) => {
+    if (num === 0) {
+      resolve('zero');
+    } else {
+      resolve('not zero');
+    }
+  })
+}
+
+withConstructor(0)
+  .then((resolveValue) => {
+    console.log(` withConstructor(0) returned a promise which resolved to: ${resolveValue}.`);
+  })
+
+// 2. async function declaration:
+async function withAsync(num) {
+  if (num === 0) {
+    return 'zero';
+  } else {
+    return 'not zero';
+  }
+}
+
+withConstructor(0)
+  .then((resolveValue) => {
+    console.log(` withConstructor(0) returned a promise which resolved to: ${resolveValue}.`);
+  })
+
+
+
+
+
+// Leave this commented out until step 3:
+/*
+withAsync(100)
+  .then((resolveValue) => {
+  console.log(` withAsync(100) returned a promise which resolved to: ${resolveValue}.`);
+})
+*/
