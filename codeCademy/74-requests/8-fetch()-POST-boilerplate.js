3c1211d619dd0d90b78312a288bfacbaaf72b8d5
Web Intern <web.intern@canucks.com>
2019-04-02 16:24:32 -0700

    Complete POST boilerplate

--- codeCademy/74-requests/8-fetch()-POST-boilerplate.js
+++ codeCademy/74-requests/8-fetch()-POST-boilerplate.js
@@ -1,1 +1,17 @@
-fetch('https://api-to-call.com/endpoint', {})
+fetch('https://api-to-call.com/endpoint', {
+    method: 'POST',
+    body: JSON.stringify({
+      id: '200'
+    })
+  })
+  .then((response) => {
+    if (response.ok) {
+      return response.json();
+    }
+    throw new Error('Request failed!');
+  }, (networkError) => {
+    console.log(networkError.message);
+  })
+  .then((jsonResponse) => {
+    // Code to execute with JSON
+  })
