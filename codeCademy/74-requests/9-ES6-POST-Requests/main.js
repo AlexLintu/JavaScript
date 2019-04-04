dd1cff97e59acfc98b44603ec4dce14ee6960b38
Alexandra Tkachenko <osatkachenko@gmail.com>
2019-04-03 20:47:04 -0700

    Add fetch request

--- codeCademy/74-requests/9-ES6-POST-Requests/main.js
+++ codeCademy/74-requests/9-ES6-POST-Requests/main.js
@@ -1,5 +1,5 @@
 // Information to reach API
-const apiKey = '<Your API Key>';
+const apiKey = '1e6ac3400aad4097b4119b9a5f325574';
 const url = 'https://api.rebrandly.com/v1/links';
 
 // Some page elements
@@ -9,6 +9,17 @@ const responseField = document.querySelector('#responseField');
 
 // AJAX functions
 const shortenUrl = () => {
+  const urlToShorten = inputField.value;
+  const data = JSON.stringify({ destination: urlToShorten });
+
+  fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-type': 'application/json',
+      'apikey': apiKey
+    },
+    body: data
+  })
 }
 
 // Clear page and call AJAX functions
