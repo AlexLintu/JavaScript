e8bc394d6ade929b56095a8cbd1830fa6deac842
Alexandra Tkachenko <osatkachenko@gmail.com>
2019-04-10 19:44:22 -0700

    Complete GET request to Foursquare

--- JavaScript-projects/38-wanderlust (Foursquare API & APIXU)/main.js
+++ JavaScript-projects/38-wanderlust (Foursquare API & APIXU)/main.js
@@ -19,14 +19,15 @@ const weekDays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Frida
 // Add AJAX functions here:
 const getVenues = async () => {
   const city = $input.val();
-  const urlToFetch = url + city + '&limit=10&client_id=' + clientId + '&client_secret=' + clientSecret + '&v=20190408';
+  const urlToFetch = url + city + '&limit=10&client_id=' + clientId + '&client_secret=' + clientSecret + '&v=20190410';
 
   try {
     const response = await fetch(urlToFetch);
 
     if (response.ok) {
       const jsonResponse = await response.json();
-      console.log(jsonResponse);
+      const venues = jsonResponse.response.groups[0].items.map(item => item.venue);
+      return venues;
     }
   }
 
@@ -34,7 +35,6 @@ const getVenues = async () => {
     console.log(error);
   }
 }
-
 const getForecast = () => {
 
 }
