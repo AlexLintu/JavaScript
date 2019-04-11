9ffecc88244065d7977b1549f658a2317cf80c6b
Web Intern <web.intern@canucks.com>
2019-04-11 16:35:43 -0700

    Add new venue

--- JavaScript-projects/38-wanderlust (Foursquare API & APIXU)/main.js
+++ JavaScript-projects/38-wanderlust (Foursquare API & APIXU)/main.js
@@ -12,7 +12,8 @@ const $input = $('#city');
 const $submit = $('#button');
 const $destination = $('#destination');
 const $container = $('.container');
-const $venueDivs = [$("#venue1"), $("#venue2"), $("#venue3"), $("#venue4")];
+// New venue
+const $venueDivs = [$("#venue1"), $("#venue2"), $("#venue3"), $("#venue4"), $("#venue5")];
 const $weatherDivs = [$("#weather1"), $("#weather2"), $("#weather3"), $("#weather4")];
 const weekDays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
