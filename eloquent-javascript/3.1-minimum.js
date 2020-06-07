39dfad0f192169ec81ede4ac2ffaf4331c1d3fd8
Alexandra Ogarok <alexandra.ogarok@gmail.com>
2020-06-07 16:28:23 -0700

    Minimum

---
+++ eloquent-javascript/3.1-minimum.js
@@ -0,0 +1,13 @@
+const min = (num1, num2) => {
+  if (num1 === num2) {
+    return 'Both numbers are equal!';
+  } else if (num1 < num2) {
+    return num1;
+  } else {
+    return num2;
+  }
+}
+
+console.log(min(10, 10)); // Both numbers are equal!
+console.log(min(0, 10)); // 0
+console.log(min(0, -10)); // -10
