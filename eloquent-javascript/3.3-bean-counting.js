8cd58feb93e7456687dba9500c2f81977d346a5b
Alexandra Ogarok <alexandra.ogarok@gmail.com>
2020-06-07 17:33:28 -0700

    Bean counting

---
+++ eloquent-javascript/3.3-bean-counting.js
@@ -0,0 +1,21 @@
+// Version 1
+const countChar = (str, char) => {
+  let charCounter = 0;
+  for (let i = 0; i <= str.length - 1; i++) {
+    if (str[i] === char) {
+      charCounter += 1;
+    }
+  }
+  return charCounter;
+}
+
+const countBs = str => {
+  return countChar(str, 'B');
+}
+
+console.log(countBs("BBCBBCB"));
+// → 2
+console.log(countChar("kakkerklak", "k"));
+// → 4
+console.log(countChar("kakkerklak", "z"));
+// → 0
