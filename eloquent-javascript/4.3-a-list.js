3ca8c2d4f5e334f8f6f27ca9072914dd30f8661e
Alexandra Ogarok <alexandra.ogarok@gmail.com>
2020-06-14 17:21:07 -0700

    Add 2 helper functions

--- eloquent-javascript/4.3-a-list.js
+++ eloquent-javascript/4.3-a-list.js
@@ -48,4 +48,29 @@ function listToArray(list) {
   return array;
 }
 
+function prepend(element, list) {
+  let newList = {
+    value: element,
+    next: list
+  }
+  return newList;
+}
+
+function nth(list, n) {
+  if (!list) {
+    return undefined;
+  } else if (n === 0) {
+    return list.value
+  } else {
+    return nth(list.next, n - 1);
+  }
+}
+
+console.log(arrayToList([10, 20]));
+// → {value: 10, rest: {value: 20, rest: null}}
 console.log(listToArray(arrayToList([10, 20, 30])));
+// → [10, 20, 30]
+console.log(prepend(10, prepend(20, null)));
+// → {value: 10, rest: {value: 20, rest: null}}
+console.log(nth(arrayToList([10, 20, 30]), 1));
+// → 20
